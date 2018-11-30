57922b1549cdb7ebdcbbd12354e6e37ab06b525c
Ian James <keldar67@gmail.com>
2018-11-30 15:48:23 +1100

    Partially updated -
    Working towards creating and opening local file rather than opening the central model.
    Link to documentation online saved at top of the script

--- BatchProcessLinkUpdates.py
+++ BatchProcessLinkUpdates.py
@@ -13,9 +13,25 @@ app = uiapp.Application
 class SynchLockCallback(ICentralLockedCallback):
   def ShouldWaitForLockAvailability():
     return False
+#--------------------------------------------------------------------------------#
+# Refer to https://knowledge.autodesk.com/search-result/caas/CloudHelp/cloudhelp/2016/ENU/Revit-API/files/GUID-F99FBBCC-AE58-46F5-85AF-0A7C3C5C0576-htm.html?_ga=2.18634043.303903109.1543365649-1865787168.1469580492
+#--------------------------------------------------------------------------------#
+def GetLocalFilePath(filename):
 
+  #If we get a filename including the ".rvt" then remove it.
+  if filename.EndsWith('.rvt'):
+    fname = filename[:-4]
+  else:
+    fname = filename
+  
+  localpath = "C:\\REVIT_LOCAL" + app.VersionNumber + "\\" + fname + "_" + app.Username + ".rvt."
+  
+  return localpath
 #--------------------------------------------------------------------------------#
 def DoIt():
+  #Handle to the Application
+  app = uiapp.Application
+  
   #Arguments required for the file open operation.
   modelPath = ModelPathUtils.ConvertUserVisiblePathToModelPath(apath)
   opts = OpenOptions()
