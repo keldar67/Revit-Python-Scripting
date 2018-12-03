6ab40064745a57aac88f7a7ec019ff6e6c79960b
Ian James <keldar67@gmail.com>
2018-12-03 21:34:48 +1100

    Existing File added to test Git

---
+++ RevitLinkListNames.py
@@ -0,0 +1,5 @@
+﻿fec = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_RvtLinks)
+for f in fec:
+  if RevitLinkInstance == type(f):
+    data = Element.Name.GetValue(f).split(':')
+    print data[0]
