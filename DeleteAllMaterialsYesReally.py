7bb1e7e1e390ff71d5966ab686b77dcd3b1bb2a1
Ian James <keldar67@gmail.com>
2018-12-04 14:45:03 +1100

    Quick Script for Shawnee to test removing materials from Family files

---
+++ DeleteAllMaterialsYesReally.py
@@ -0,0 +1,15 @@
+﻿import collections
+
+theMaterials = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Materials).ToElementIds()
+#for aMaterial in theMaterials:
+
+tranny = Transaction(doc)
+try:
+  tranny.Start('Deleting the Materials ')
+  doc.Delete(theMaterials)
+  tranny.Commit()
+  tranny.Dispose()
+except Exception as e:
+  tranny.RollBack()
+  tranny.Dispose()
+  print e.message
