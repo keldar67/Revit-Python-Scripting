f9b230dc2e70a31719fe6d5fb716a39418be50d8
Ian James <keldar67@gmail.com>
2018-12-08 10:21:29 +1100

    Fixed file paths and also added function to skip files already repathed

--- RePathLinksPostUpgrade.py
+++ RePathLinksPostUpgrade.py
@@ -30,6 +30,7 @@ BVNProjfiles = {
 'AR-MOD-XX-XX-010': os.path.join(projfilesfolder,'AR-MOD-XX-XX-010-RVT - Master Grid.rvt'),
 'AR-MOD-XX-XX-015': os.path.join(sitefilesfolder,'AR-MOD-XX-XX-015-RVT - Clearance Zones.rvt'),
 'AR-MOD-XX-XX-020': os.path.join(sitefilesfolder,'AR-MOD-XX-XX-020-RVT - DCP Massing.rvt'),
+'AR-MOD-XX-XX-025': os.path.join(sitefilesfolder,'AR-MOD-XX-XX-025-RVT - 33 Alfred St.rvt'),
 'AR-MOD-XX-XX-800': os.path.join(sitefilesfolder,'AR-MOD-XX-XX-800-RVT - MPX Site.rvt'),
 'AR-MOD-XX-XX-900': os.path.join(sitefilesfolder,'AR-MOD-XX-XX-900-RVT - Enscape 3D.rvt'),
 'AR-MOD-XX-XX-999': os.path.join(areafilesfolder,'AR-MOD-XX-XX-999-RVT - Area Analysis Model.rvt')
@@ -37,7 +38,7 @@ BVNProjfiles = {
 
 ACONEXProjFiles = {
 #ASPECT
-'LA-MOD_XX_XX_001': r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\ASPECT\LA-MOD-XX-XX-001-RVT-17 - Landscape.rvt',
+'LA-MOD-XX-XX-001': r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\ASPECT\LA-MOD-XX-XX-001-RVT-17 - Landscape.rvt',
 #AXIS
 'AXS-HY-MOD-XX-001': r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\AXIS\AXS-HY-MOD-XX-001-RVT-27 - Hydraulic Services.rvt',
 #BG&E
@@ -66,49 +67,58 @@ ACONEXProjFiles = {
 }
 
 otherProjLinks = {
+#BVN
 'Site Context.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\BVN\Site Context\Site Context.rvt',
 '33 Alfred Interface.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\4.0 CAD Links\1.0 MPX\BVN\33 Alfred Interface.rvt',
+#AAM
 'SU-MOD-XX-XX-001 - AAM Survey.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\4.0 CAD Links\1.0 MPX\AAM\SU-MOD-XX-XX-001 - AAM Survey.rvt',
+#AvroKO
 'AvroKO - CAD Links.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\4.0 CAD Links\2.0 AMPC\AvroKO\AvroKO - CAD Links.rvt',
+#WEBB
 'WEBB Substations CAD.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\4.0 CAD Links\1.0 MPX\WEBB\WEBB Substations CAD.rvt',
-'SE-ST-MOD - Levels B05 - L02.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\VANDEMEER\001 - Levels B5 - 02\SE-ST-MOD - Levels B05 - L02.rvt',
-'SE-ST-MOD - Levels L02 - L06.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\VANDEMEER\002 - Levels 02 - 06\SE-ST-MOD - Levels L02 - L06.rvt',
-'SE-ST-MOD - Levels L07 - L14.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\VANDEMEER\003 - Levels 07 - 14\SE-ST-MOD - Levels L07 - L14.rvt',
-'SE-ST-MOD - Levels L15 - L20.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\VANDEMEER\004 - Levels 15 - 20\SE-ST-MOD - Levels L15 - L20.rvt',
+#Van der Meer
+'SSE-ST-MOD - Levels B05 - L02.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\VANDERMEER\001 - Levels B5 - 02\SSE-ST-MOD - Levels B05 - L02.rvt',
+'SSE-ST-MOD - Levels L02 - L06.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\VANDERMEER\002 - Levels 02 - 06\SSE-ST-MOD - Levels L02 - L06.rvt',
+'SSE-ST-MOD - Levels L07 - L14.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\VANDERMEER\003 - Levels 07 - 14\SSE-ST-MOD - Levels L07 - L14.rvt',
+'SSE-ST-MOD - Levels L15 - L20.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\VANDERMEER\004 - Levels 15 - 20\SSE-ST-MOD - Levels L15 - L20.rvt',
+#LTS
 'LTS Survey Plans.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\4.0 CAD Links\1.0 MPX\LTS\LTS Survey Plans\LTS Survey Plans.rvt',
 'LTS Survey South Stairs.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\4.0 CAD Links\1.0 MPX\LTS\LTS South Stair\LTS Survey South Stairs.rvt',
+'LTS SOUTH STAIR SURVEY.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\4.0 CAD Links\1.0 MPX\LTS\LTS South Stair\LTS Survey South Stairs.rvt',
+#DRS CAD
 'DRS - CAD Links.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\4.0 CAD Links\2.0 AMPC\DRS\DRS - CAD Links.rvt',
+#ASPECT
 'ASPECT Site Background.rvt': r'X:\003 - Multiplex\Design\BIM\_Revit\4.0 CAD Links\1.0 MPX\ASPECT\Site Background\ASPECT Site Background.rvt'
 }
-
-
-
 #'': r'',
 
-# Is this link a BVN Project Link
-# Look in BVNProjFiles dict using the full filename (No revision required)
-# if '-RVT - ' in LinkName:
-#   pass
-
-
-# Is this link an Aconex Shared Project Link
-# Look in the ACONEXProjFiles dict and do a lookup for latest revision
-# if '-RVT-' in LinkName
-#   pass
-
-# If not then look in the last dict using full filename as key
-# No revision
 #------------------------------------------------------------------------#
 def GetLinkName(aLink):
   #return [x.strip() for x in Element.Name.GetValue(aLink).split(':')]
   return Element.Name.GetValue(aLink)
 #------------------------------------------------------------------------#
+def LinkAlreadyPathed(aLink, thePath):
+  result = False
+  #Grab the Model Path of the Link
+  er = aLink.GetExternalFileReference()
+  mp = er.GetPath()
+  #Convert it so we can read it
+  path = ModelPathUtils.ConvertModelPathToUserVisiblePath(mp)
+  #This will be a UNC path so convert it to the X: drive path
+  xpath = os.path.splitunc(path)[1].replace('\\13\\s1309002.yel','X:')
+  #Check if they point to the same file 
+  if xpath == thePath:
+    result = True
+    
+  return result
+  
+#------------------------------------------------------------------------#
 def GetAllLinks():
   theLinks = (
     FilteredElementCollector(doc)
     .OfCategory(BuiltInCategory.OST_RvtLinks)
-    #Only Link Types, NOT Link Instances
-    .Where(lambda link: RevitLinkType == type(link))
+    #Only Link Types, NOT a Nested Link
+    .Where(lambda link: (RevitLinkType == type(link)) and not(link.IsNestedLink))
     )
     
   return theLinks.ToList()
@@ -121,11 +131,11 @@ def RePathBVNModel(aLink):
   keyval = linkName[:16]
    
   #Get the New Path from the Dictionary above
-  newPath = BVNProjfiles[keyval]
-
-  if newPath:
+  if keyval in BVNProjfiles.Keys:
+    newPath = BVNProjfiles[keyval]
     return RePathLinks(aLink, newPath)
   else:
+    print 'Key Not Found: ' + keyval
     return False
 #------------------------------------------------------------------------# 
 def GetLatestRevisionFromFolder(fileID, folderpath):
@@ -154,7 +164,7 @@ def RePathConsSubModel(aLink):
   #Grab the first 16/17 characters of the filename up to but not
   #including the "-RVT"
   keyval = linkName[:linkName.find('-RVT')]
-  
+
   try:
     #Get the New Path from the Dictionary above
     newPath = ACONEXProjFiles[keyval]
@@ -188,16 +198,22 @@ def RePathGeneralModel(aLink):
 
 #------------------------------------------------------------------------# 
 def RePathLinks(aLink, newPath):
-  modelpath = ModelPathUtils.ConvertUserVisiblePathToModelPath(newPath)
-  try:
-    reloadResults = aLink.LoadFrom(modelpath, WorksetConfiguration())
-    return True
-  except Exception as e:
-    print 'Error Repathing ' + GetLinkName(aLink)
-    print newPath
-    print e.message
-    print '-' * 75
-    return False
+  # Check to make sure that the file needs repathing first
+  if LinkAlreadyPathed(aLink, newPath):
+    print 'ALREADY CURRENT: ' + GetLinkName(aLink)
+  else:
+    modelpath = ModelPathUtils.ConvertUserVisiblePathToModelPath(newPath)
+    try:
+      # Re-Path the file
+      reloadResults = aLink.LoadFrom(modelpath, WorksetConfiguration())
+      return True
+    except Exception as e:
+      print 'Error Repathing ' + GetLinkName(aLink)
+      print newPath
+      print e.message
+      print '-' * 75
+      return False
+  
 #------------------------------------------------------------------------#
 def GetLinkFolderPath(aLink):
   thePath = ''
@@ -205,7 +221,7 @@ def GetLinkFolderPath(aLink):
   er = aLink.GetExternalFileReference()
   #Make sure it isn't Null
   if not(er):
-    print Element.Name.GetValue(aLink)
+    print GetLinkName(aLink)
   else:
     #Get the Model Path
     mp = er.GetPath()
