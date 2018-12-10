5a125ecb8e50fc18b76d930dbe8dc8544062b5e0
Ian James <keldar67@gmail.com>
2018-12-10 14:10:42 +1100

    New File to list Revit Links and their Worksets

---
+++ ListRevitLinks&Worksets.py
@@ -0,0 +1,27 @@
+﻿
+#------------------------------------------------------------------------#
+def GetAllLinks():
+  theLinks = (
+    FilteredElementCollector(doc)
+    .OfCategory(BuiltInCategory.OST_RvtLinks)
+    #Only Link Types, NOT a Nested Link
+    .Where(lambda link: (RevitLinkType == type(link)) and not(link.IsNestedLink))
+    .OrderBy(lambda link: Element.Name.GetValue(link))
+    )
+    
+  return theLinks.ToList()
+#------------------------------------------------------------------------#
+def GetLinkName(aLink):
+  #return [x.strip() for x in Element.Name.GetValue(aLink).split(':')]
+  return Element.Name.GetValue(aLink)
+#------------------------------------------------------------------------#
+
+theLinks = GetAllLinks()
+wst = doc.GetWorksetTable()
+
+for aLink in theLinks:
+  linkname = GetLinkName(aLink)
+  wsetname = wst.GetWorkset(aLink.WorksetId).Name
+  print linkname + '\t\t' + wsetname
+  #print 
+  #print 50 * '-'
