8d98d7a17583dda11c76236b1e055f7c3eb94e98
Ian James <keldar67@gmail.com>
2018-12-11 14:21:49 +1100

    Intial dive into driving Outlook from Revit Python for the purpose of nitifications

---
+++ OfficeInteropOutlookExperiments_01.py
@@ -0,0 +1,10 @@
+﻿import clr
+clr.AddReference('Microsoft.Office.Interop.Outlook')
+import Microsoft.Office.Interop.Outlook as Outlook
+
+olApp = Outlook.ApplicationClass()
+from System.Runtime.InteropServices import Marshal
+outlook = Marshal.GetActiveObject("Outlook.Application")
+
+mapi = outlook.GetNamespace("MAPI")
+print "Current User: " + mapi.CurrentUser.Name
