77e67e3cfbe92ab7dd800445766c7ed7351fecb0
Ian James <keldar67@gmail.com>
2018-12-12 20:32:20 +1100

    Little Script to report on pre-selected elements

---
+++ WorksharingToolTextInfo.py
@@ -0,0 +1,11 @@
+﻿if selection.Count > 0:
+  for s in selection:
+    #Get the Worksharing Info
+    wsi = WorksharingUtils.GetWorksharingTooltipInfo(doc,s.Id)
+    
+    print s.Category.Name + ': ' + Element.Name.GetValue(s)
+    print '[' + s.Id.ToString() + ']'
+    print 'Created By      : ' + wsi.Creator
+    print 'Last Changed By : ' + wsi.LastChangedBy
+    print 'Owner           : ' + wsi.Owner
+    print '-' * 75
