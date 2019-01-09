82a99119ff6e50887afdb1f57b8fca96b4557a60
Ian James <keldar67@gmail.com>
2019-01-09 13:59:06 +1100

    Success getting to send email from python shell... needs user to accept in outlook

--- OfficeInteropOutlookExperiments_01.py
+++ OfficeInteropOutlookExperiments_01.py
@@ -2,9 +2,15 @@
 clr.AddReference('Microsoft.Office.Interop.Outlook')
 import Microsoft.Office.Interop.Outlook as Outlook
 
-olApp = Outlook.ApplicationClass()
+#olApp = Outlook.ApplicationClass()
 from System.Runtime.InteropServices import Marshal
-outlook = Marshal.GetActiveObject("Outlook.Application")
+#outlook = Marshal.GetActiveObject("Outlook.Application")
 
-mapi = outlook.GetNamespace("MAPI")
-print "Current User: " + mapi.CurrentUser.Name
+#mapi = outlook.GetNamespace("MAPI")
+#print "Current User: " + mapi.CurrentUser.Name
+
+newemail = Marshal.GetActiveObject('Outlook.Application').CreateItem(0)
+newemail.Recipients.Add('ian_james@bvn.com.au')
+newemail.Subject = 'This is a test'
+newemail.Body = 'This is a test email\nIt should have some text in the body'
+newemail.Send()
