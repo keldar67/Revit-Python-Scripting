fa3cab742374b591929e1383b2b6410341519ca7
Ian James <keldar67@gmail.com>
2019-01-16 14:06:39 +1100

    First Commit, Experimentation creating Log Files and opening in Notepad

---
+++ WritingTempFileLog.py
@@ -0,0 +1,24 @@
+﻿import os
+import os.path
+import time, datetime
+import subprocess as sp
+
+def GetRevitVersionNumber():
+  return uiapp.Application.VersionNumber
+
+theversion = GetRevitVersionNumber()
+theModelName = doc.Title[:doc.Title.LastIndexOf('_')]
+timestamp = datetime.datetime.now().strftime("(%Y-%m-%d - %H-%M)")
+thePath = r'C:\Revit_Local' + theversion + '\\' + theModelName + '_ReloadLinks_' + timestamp + '.log'
+
+print thePath
+
+#Open, Write to the File & Close
+fp = open(thePath, 'w')
+fp.write('Hello World')
+fp.close()
+
+#Then Open in Notepad.exe
+NotepadCommandString = 'notepad.exe' 
+logfile = thePath
+sp.Popen([NotepadCommandString,logfile])
