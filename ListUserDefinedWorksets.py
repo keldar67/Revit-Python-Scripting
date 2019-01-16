f223b7235d248b94012e5f6e2285adbfdc7c5f8d
Ian James <keldar67@gmail.com>
2019-01-16 11:30:54 +1100

    First Upload - Lists User Worksets in a formatted list Id and Name

---
+++ ListUserDefinedWorksets.py
@@ -0,0 +1,11 @@
+﻿wst = doc.GetWorksetTable()
+
+theWorksets = (
+FilteredWorksetCollector(doc)
+.Where(lambda w: w.Kind == WorksetKind.UserWorkset)
+)
+
+print ' +------------+-------------------------------------------+'
+for aWorkset in theWorksets:
+  print ' | ' + aWorkset.Id.ToString().ljust(10) + ' | ' + aWorkset.Name
+print ' +------------+-------------------------------------------+'
