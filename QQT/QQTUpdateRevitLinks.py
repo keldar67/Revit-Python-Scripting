9ea29a001866c717ef21cd5d7638f76fa102e944
Ian James <kelar67@gmail.com>
2019-09-04 18:30:48 +1000

    QQT UpdateLinks updated to account for the P Drive Migration

--- QQT/QQTUpdateRevitLinks.py
+++ QQT/QQTUpdateRevitLinks.py
@@ -13,6 +13,10 @@
 #
 #  January 2019
 #
+# Updated September 2019 to take account of the relocation of the project
+# from the x: drive to the p: drive as part of the 
+# Sydney P: Drive Migration
+#
 #========================================================================#
 import os
 import os.path
@@ -192,7 +196,7 @@ def CreateLogFile(logtext):
   theversion = uiapp.Application.VersionNumber
   theModelName = doc.Title[:doc.Title.LastIndexOf('_')]
   timestamp = datetime.datetime.now().strftime("(%Y-%m-%d - %H-%M-%S)")
-  thePath = r'X:\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\000 LOGS' + '\\' + theModelName + '_ReloadLinks_' + timestamp + '.log'
+  thePath = r'P:\13\s1309002.yel\003 - Multiplex\Design\BIM\_Revit\8.0 Revit Links\1.0 MPX\000 LOGS' + '\\' + theModelName + '_ReloadLinks_' + timestamp + '.log'
 
   #Open, Write to the File & Close
   fp = open(thePath, 'w')
