229af47f2ce61abb78f492570753f1e9c074d16c
Ian James <Ian_James@bvn.com.au>
2020-01-20 14:59:24 +1100

    First Commit - Structural Columns Only ATM

---
+++ StructuralAnalyticsOFF.py
@@ -0,0 +1,32 @@
+﻿theColumns = (
+  FilteredElementCollector(doc)
+  .OfCategory(BuiltInCategory.OST_StructuralColumns)
+  .ToList()
+)
+i = 0
+count = theColumns.Count.ToString()
+
+for aColumn in theColumns:
+  i += 1
+  print i.ToString() + ' of ' + count + ' Columns'
+  id = aColumn.Id.ToString()
+  
+  p = aColumn.LookupParameter('Enable Analytical Model')
+  if p:
+    print 'Element Id [' + id + '] Changing From: ' + p.AsValueString() + 'No/False'
+    tranny = Transaction(doc)
+    try:
+      tranny.Start('Switching Off analytics')
+      p.Set(False)
+      tranny.Commit()
+      tranny.Dispose()
+      print 'Success!'
+    except Exception as e:
+      if tranny.HasStarted:
+        tranny.RollBack()
+        tranny.Dispose()
+      print 'Something Went Wrong'
+      print e.message
+  else:
+    print "Column [" + id + "] Doesn't have the parameter"
+    
